2e7dc4aa133fb57c0ee33695e39425f02751f160
Aaron Llasat <94169219+AaronLlasat@users.noreply.github.com>
2024-09-18 11:51:42 +0200

    Update OnRender

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch("https://salty-river-25477.herokuapp.com/signin", {
+        fetch("https://smart-brain-api-a3yx.onrender.com/signin", {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
